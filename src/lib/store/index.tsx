67b400af350927ffef74a52403002fddfdc2fa04
Theophilus Johnson <>
2026-01-13 16:31:32 +0100

    feat: add form-builder-context

--- src/lib/store/index.tsx
+++ src/lib/store/index.tsx
@@ -1,4 +1,13 @@
 import { useContext } from 'react';
 import { AppContext } from './app-context';
+import { FormBuilderContext } from './form-builder-context';
+
+export const useFormBuilder = () => {
+  const context = useContext(FormBuilderContext);
+  if (!context) {
+    throw new Error('useFormBuilder must be used within FormBuilderProvider');
+  }
+  return context;
+};
 
 export const useAppStore = () => useContext(AppContext);
