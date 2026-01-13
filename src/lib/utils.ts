860ab621c417706c181a8ad886a0e8855b9154ac
Theophilus Johnson <>
2026-01-13 15:40:54 +0100

    feat: update utils func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const generateId = () =>
+  `field_${Date.now()}_${Math.random().toString(36).substr(2, 9)}`;
