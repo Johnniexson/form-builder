3015f0202a02df59ad066d26a72ee68cd0bece8f
Theophilus Johnson <>
2026-01-13 15:49:57 +0100

    feat: add label component

---
+++ src/components/ui/label.tsx
@@ -0,0 +1,28 @@
+import type * as React from 'react';
+
+import { cn } from '@/lib/utils';
+
+function Label({
+  htmlFor,
+  className,
+  children,
+  required,
+  ...props
+}: React.ComponentProps<'label'> & { required?: boolean }) {
+  return (
+    <label
+      htmlFor={htmlFor}
+      data-slot="label"
+      className={cn(
+        'flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
+        className
+      )}
+      {...props}
+    >
+      {children}
+      {required && <span className="text-red-500">*</span>}
+    </label>
+  );
+}
+
+export { Label };
